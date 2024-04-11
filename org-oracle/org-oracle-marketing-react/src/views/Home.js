de1ce1396e833160c27677de6fabac1303634893
AnnMary Mathew <amm@AnnMarys-MacBook-Pro.local>
2024-04-11 17:49:58 -0400

    initial commit

---
+++ org-oracle/org-oracle-marketing-react/src/views/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HeroSplit from '../components/sections/HeroSplit';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesTabs from '../components/sections/FeaturesTabs';
+import News from '../components/sections/News';
+import Roadmap from '../components/sections/Roadmap';
+import Pricing from '../components/sections/Pricing';
+import Cta from '../components/sections/Cta';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <HeroSplit hasBgColor invertColor />
+        <FeaturesTiles />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
